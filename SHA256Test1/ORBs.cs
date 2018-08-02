379d0b9d308affb6e78d6d656adef1f2e730910f
Michael Herman <mwherman2000@hotmail.com>
2018-08-02 17:37:06 -0400

    Updates

---
+++ SHA256Test1/ORBs.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHA256Test1
+{
+    public partial class Terms
+    {
+       public static string[] ORBs =
+       {
+            "Root",
+            "Position & Title",
+            "Ecological Tokens",
+            "Products",
+            "Loyalty Tokens",
+            "For-profit Companies",
+            "People",
+            "Reputation Tokens",
+            "Non-Profit/Charitable Organizations",
+            "Schools",
+            "Keywords",
+            "Services",
+            "Interest & Dividend Tokens",
+            "Generic Entity",
+            "Organizations",
+            "Roles",
+            "Philanthropic Supporter",
+            "Open Source Contributor",
+            "Investments",
+            "Locations",
+            "Knowledge & Experience",
+            "Process Instances",
+            "Skills",
+            "Processes, Activities & Tasks",
+            "Policies, Procedures & Best Practices",
+            "Currency Tokens",
+            "Regulations & Laws",
+            "Value Tokens",
+            "Offerings",
+            "Governance",
+            "Events",
+            "Course",
+            "Certification",
+            "Webcast",
+            "Seminar",
+            "Conference",
+            "Meetup",
+            "Strategy Elements",
+            "Purpose",
+            "Goal",
+            "Objective",
+            "Driver",
+            "Requirement",
+            "Constraint",
+            "Outcome",
+            "Assessment",
+        };
+    }
+}
